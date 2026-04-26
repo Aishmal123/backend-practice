072432175a733aa75ab5f020d6cb08ac7d14c90a
Aishmal123 <166380416+Aishmal123@users.noreply.github.com>
2026-04-26 20:56:02 +0500

    introduction to express

---
+++ intro2express.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const port =3000;
+const app=express();
+app.use(express.json());
+app.get('/',(req,res)=>{
+    console.log('you are learning express');
+    res.send('you are achieving it');
+    
+});
+app.get('/about',(req,res)=>{
+    console.log("here is the basic information of your about page")
+    res.send("here is the basic information of your about page")
+})
+app.get('/contact',(req,res)=>{
+       console.log("here is the basic information of your about page")
+    res.send("here is the basic information of your contact page")
+})
+app.listen(port,()=>{
+    console.log('you are on the right path')
+});
